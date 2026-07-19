c4d040956e2ad33b61beb6f8fd956250925d647e
Matsu <dustinhallock@gmail.com>
2026-07-19 07:40:01 -0400

    Fix infinite science pack duplication via Recycler
    Conversion recipes were being picked up by Space Age's automatic
    recycling recipe generation, letting the Recycler bypass configured
    conversion ratios and create free duplication loops between adjacent
    science tiers. Setting auto_recycle = false on all conversion recipes
    prevents this.

--- prototypes/recipe.lua
+++ prototypes/recipe.lua
@@ -5,6 +5,7 @@ local base_recipes = {
       name = "green-to-red",
       energy_required = 10,
       enabled = false,
+      auto_recycle = false,
       ingredients = {
         {type = "item", name = "logistic-science-pack", amount = 1}
       },
@@ -17,6 +18,7 @@ local base_recipes = {
       name = "red-to-green",
       energy_required = 10,
       enabled = false,
+      auto_recycle = false,
      ingredients = {
             {type = "item", name = "automation-science-pack", amount = 10}
         },
@@ -29,6 +31,7 @@ local base_recipes = {
       name = "green-to-blue",
       energy_required = 10,
       enabled = false,
+      auto_recycle = false,
       ingredients = {
         {type = "item", name = "logistic-science-pack", amount = 20}
       },
@@ -41,6 +44,7 @@ local base_recipes = {
       name = "blue-to-green",
       energy_required = 10,
       enabled = false,
+      auto_recycle = false,
       ingredients = {
         {type = "item", name = "chemical-science-pack", amount = 1}
       },
@@ -53,6 +57,7 @@ local base_recipes = {
       name = "blue-to-purple",
       energy_required = 10,
       enabled = false,
+      auto_recycle = false,
       ingredients = {
         {type = "item", name = "chemical-science-pack", amount = 30}
       },
@@ -65,6 +70,7 @@ local base_recipes = {
       name = "purple-to-blue",
       energy_required = 10,
       enabled = false,
+      auto_recycle = false,
       ingredients = {
         {type = "item", name = "production-science-pack", amount = 1}
       },
@@ -77,6 +83,7 @@ local base_recipes = {
       name = "purple-to-yellow",
       energy_required = 10,
       enabled = false,
+      auto_recycle = false,
       ingredients = {
         {type = "item", name = "production-science-pack", amount = 50}
       },
@@ -89,6 +96,7 @@ local base_recipes = {
       name = "yellow-to-purple",
       energy_required = 10,
       enabled = false,
+      auto_recycle = false,
       ingredients = {
         {type = "item", name = "utility-science-pack", amount = 1}
       },
@@ -105,6 +113,7 @@ local space_age_recipes = {
       name = "white-to-orange",
       energy_required = 10,
       enabled = false,
+      auto_recycle = false,
       ingredients = {
         {type = "item", name = "space-science-pack", amount = 100}
       },
@@ -117,6 +126,7 @@ local space_age_recipes = {
       name = "orange-to-white",
       energy_required = 10,
       enabled = false,
+      auto_recycle = false,
       ingredients = {
         {type = "item", name = "metallurgic-science-pack", amount = 1}
       },
@@ -129,6 +139,7 @@ local space_age_recipes = {
       name = "white-to-pink",
       energy_required = 10,
       enabled = false,
+      auto_recycle = false,
       ingredients = {
         {type = "item", name = "space-science-pack", amount = 100}
       },
@@ -141,6 +152,7 @@ local space_age_recipes = {
       name = "pink-to-white",
       energy_required = 10,
       enabled = false,
+      auto_recycle = false,
       ingredients = {
         {type = "item", name = "electromagnetic-science-pack", amount = 1}
       },
@@ -153,6 +165,7 @@ local space_age_recipes = {
       name = "white-to-lime",
       energy_required = 10,
       enabled = false,
+      auto_recycle = false,
       ingredients = {
         {type = "item", name = "space-science-pack", amount = 100}
       },
@@ -165,6 +178,7 @@ local space_age_recipes = {
       name = "lime-to-white",
       energy_required = 10,
       enabled = false,
+      auto_recycle = false,
       ingredients = {
         {type = "item", name = "agricultural-science-pack", amount = 1}
       },
@@ -177,6 +191,7 @@ local space_age_recipes = {
       name = "white-to-dark-blue",
       energy_required = 10,
       enabled = false,
+      auto_recycle = false,
       ingredients = {
         {type = "item", name = "space-science-pack", amount = 100}
       },
@@ -189,6 +204,7 @@ local space_age_recipes = {
       name = "dark-blue-to-white",
       energy_required = 10,
       enabled = false,
+      auto_recycle = false,
       ingredients = {
         {type = "item", name = "cryogenic-science-pack", amount = 1}
       },
@@ -201,6 +217,7 @@ local space_age_recipes = {
       name = "white-to-black",
       energy_required = 10,
       enabled = false,
+      auto_recycle = false,
       ingredients = {
         {type = "item", name = "space-science-pack", amount = 100}
       },
@@ -213,6 +230,7 @@ local space_age_recipes = {
       name = "black-to-white",
       energy_required = 10,
       enabled = false,
+      auto_recycle = false,
       ingredients = {
         {type = "item", name = "promethium-science-pack", amount = 1}
       },
